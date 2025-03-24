5828dbd8cbece1aa22ef3b074be94cc66597c9a2
HKang0302 <heejin444@gmail.com>
2025-03-24 17:25:17 +0900

    change: darkmode ui

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -177,7 +177,10 @@ class _HomePageState extends State<HomePage> {
                 const SizedBox(height: 20),
                 Container(
                   decoration: BoxDecoration(
-                    color: Colors.white,
+                    color:
+                        Theme.of(context).brightness == Brightness.dark
+                            ? Colors.grey[800]!
+                            : Colors.white,
                     borderRadius: BorderRadius.circular(20),
                   ),
                   child: Padding(
